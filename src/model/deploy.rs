5f7c9148d9a59ceb05704fa8ab644763272e69f5
Kurt Wolf <kurtwolfbuilds@gmail.com>
2024-09-29 00:09:37 -0700

    fix deploy

--- src/model/deploy.rs
+++ src/model/deploy.rs
@@ -2,16 +2,15 @@
 use serde::{Serialize, Deserialize};
 use super::Commit;
 #[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Deploy {
-    pub commit: Commit,
-    #[serde(rename = "createdAt")]
+    #[serde(default)]
+    pub commit: Option<Commit>,
     pub created_at: chrono::DateTime<chrono::Utc>,
-    #[serde(rename = "finishedAt")]
     #[serde(skip_serializing_if = "Option::is_none")]
     pub finished_at: Option<chrono::DateTime<chrono::Utc>>,
     pub id: String,
     pub status: String,
-    #[serde(rename = "updatedAt")]
     pub updated_at: chrono::DateTime<chrono::Utc>,
 }
 impl std::fmt::Display for Deploy {
